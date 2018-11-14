bda5fc015c6b387d5b11b602e4f6512c55a71c9a
Chris Murray <murray_christopher_2010@yahoo.co.uk>
2018-11-14 21:27:13 +0000

    removed function call

--- byotest.py
+++ byotest.py
@@ -1,6 +1,3 @@
-def number_of_evens(numbers):
-    return 0
-
 def tests_are_equal(actual, expected):
     assert expected == actual, "Expected {0} got {1}".format(expected, actual)
  
@@ -16,13 +13,3 @@ def tests_not_in(collection, item):
 def tests_between(upper, lower, value):
     assert value <= upper, "{0} is greater than {1}".format(value, upper)
     assert value >= lower, "{0} is less than {1}".format(value, lower)
-    
-#tests_are_equal(number_of_evens([1,2,3,4,5]),2)
-
-#tests_not_equal(number_of_evens([1,2,3,4,5]),0)
-
-#tests_is_in([1,2,3,4,5], 7)
-
-#tests_not_in([1,2,3,4,5], 1)
-
-tests_between(5, 3, 2)
